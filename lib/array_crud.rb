6ac8e6c82ca10285c170ba3656491970b1b2d708
Ryan Gilfillan <regilfillan@gmail.com>
2019-04-21 17:05:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  sisters = ["Kendall", "Cameron", "Rachel", "Katherine"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  sisters.push["Laura"]
 end
 
 def add_element_to_start_of_array(array, element)
